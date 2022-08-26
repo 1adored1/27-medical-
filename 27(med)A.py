668579ff3ce8f648532da9abc3d68baf74270067
adored <perekrutq@gmail.com>
2022-08-26 20:44:23 +0500

    Только для файла A

---
+++ 27(med)A.py
@@ -0,0 +1,20 @@
+f = open('26.txt')
+n = int(f.readline())
+length = []
+value = []
+k = 36
+for i in range(n):
+    x, y = map(int, f.readline().split())
+    length.append(x)
+    value.append(y // k if y % k == 0 else y // k + 1)
+m = float('inf')
+x = 0
+for i in range(n):
+    s0 = length[i]
+    m0 = 0
+    for j in range(n):
+        m0 += value[j] * abs(length[j] - s0)
+    if m0 < m:
+        m = m0
+        count = i + 1
+print(m)
